dd8ef9c3c11d2920fb407093656e5828aab3b613
strikhar <illya_strikhar@yahoo.com>
2025-06-22 00:29:59 +0300

    feat: setup project structure and build homepage

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/icons', express.static(path.join(__dirname, 'icons')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+// Home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
